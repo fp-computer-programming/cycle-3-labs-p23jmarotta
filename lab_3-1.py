db9ff7098d6d9a4c7372aef2331e37176722374f
Joseph Marotta <p23jmarotta@fairfieldprep.org>
2022-09-27 10:38:33 -0400

    Added 3-1 and 3-2

---
+++ lab_3-1.py
@@ -0,0 +1,9 @@
+# Creator JM 9/27/22 lab 3-1
+
+magic_charge = 100
+shield_charge = 80
+
+if (magic_charge < 90) or (shield_charge < 75):
+   print ("The dragon burns you to a crisp.")
+else:
+   print ("You defeated the dragon! But the princess is in another castle.")
---
+++ lab_3-2.py
@@ -0,0 +1,11 @@
+# Creator JM 9/27/22 lab 3-2
+
+# int(a) # NameError
+
+# int('a') # ValueError
+
+# 'a' + 2 # TypeError
+
+# import date # ModuleNotFoundError
+
+# print("I'm a happy camper!) # SyntaxError
